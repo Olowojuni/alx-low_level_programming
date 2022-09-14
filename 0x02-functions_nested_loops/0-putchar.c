adb224a8f03d1667123623cebe980ebe8d3d733a
Olowojuni <olowojuniafolabi@gmail.com>
2022-09-14 00:07:20 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,35 +1,22 @@
-#include <unistd.h>
+#include "main.h"
+#include <string.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - Entry point
  *
- * Return: On success 1
- * On error, -1 is returned, and errno is set appropriately.
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
  */
-int _putchar(char c)
+
+int main(void)
 {
-	return (write(1, &c, 1));
+	char str[] = "_putchar";
+	int ch;
+
+for (ch = 0; ch < 8; ++ch)
+_putchar(str[ch]);
+_putchar('\n');
+
+return (0);
 }
-
-vi main.h
-#ifndef MAIN_H
-#define MAIN_H
-
-#include<stdio.h>
-
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-
-#endif /* MAIN_H */
