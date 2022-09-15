0933793dd09eff9018d291771db27329282e3c37
Olowojuni <olowojuniafolabi@gmail.com>
2022-09-15 16:28:00 -0700

    nested loop

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,31 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char c);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_square(int size);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+#endif
+
+create _putchar.c
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
