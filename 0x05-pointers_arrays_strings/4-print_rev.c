662cf39c0bbd96f890267e91e212ca3644596c11
Olowojuni <olowojuniafolabi@gmail.com>
2022-09-20 13:46:31 -0700

    wow

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -15,6 +15,7 @@ for (index = 0; s[index] != '\0'; ++index)
 	;
 
 	/*print char from the last index as you decrement*/
-for (--index; index >= 0; --index
+for (--index; index >= 0; --index)
 			_putchar(s[index]);
+_putchar('\n');
 }
