bda1512b5e65f6a7a246f70cfce62fabc46732ad
Olowojuni <olowojuniafolabi@gmail.com>
2022-09-22 15:18:31 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates the string pointed to by @scr to
+ * the end of the string pointed to by @dest.
+ *
+ * @dest: string that will be appended
+ * @scr: string to be concatenated upon
+ *
+ * Return: returns poiner to @dest
+ */
+
+char *_strcat(char *dest, char *scr)
+{
+
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; scr[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
