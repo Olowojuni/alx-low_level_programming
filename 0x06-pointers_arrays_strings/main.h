5fa4e3a259ba461308740d595ba4952772888dfe
Olowojuni <olowojuniafolabi@gmail.com>
2022-09-22 15:26:06 -0700

    task 1

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 #include <stdio.h>
 
-int _putchar(char c);
 void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
@@ -10,10 +9,10 @@ int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *scr);
 char *strncat(char *dest, char *scr, int n);
 char *_strncpy(char *dest, char *scr, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
-char *rot13(char *);
+char *string_toupper(char *a);
+char *cap_string(char *a);
+char *leet(char *a);
+char *rot13(char *a);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 
